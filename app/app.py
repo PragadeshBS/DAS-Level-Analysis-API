5509287bf163fefd5da0e947e6c50022b05999f6
Pragadesh BS <pragadeshbs@protonmail.com>
2023-11-23 11:30:01 +0530

    add qs answer api

--- app/app.py
+++ app/app.py
@@ -1,19 +1,52 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
 from app import dsa
-import os
 import requests
+from dotenv import load_dotenv
+import os
 
-bp=Blueprint('app',__name__)
+load_dotenv()
 
-@bp.route('/')
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-distilled-squad"
+headers = {"Authorization": f"Bearer {os.getenv('HUGGINGFACE_API_KEY')}"}
+
+bp = Blueprint("app", __name__)
+
+
+def answer_based_on_context(question):
+    payload = {
+        "inputs": {
+            "question": question,
+            "context": "A crowded railway station is a hub of activity, a microcosm of life itself. The station is bustling with people from all walks of life, each with their own story to tell. The air is filled with the cacophony of sounds - the chatter of passengers, the loudspeaker announcements, the clatter of luggage, and the occasional whistle of a train pulling into or out of the station. The ticket counter, located inside the station building right next to the platform, is a scene of organized chaos. There's a long queue of passengers waiting patiently to buy their tickets, while the ticketing staff work efficiently, issuing tickets and providing information to the travelers. The platform itself is a hive of activity. Passengers are scattered everywhere - some are waiting for their trains, others are saying their goodbyes, while a few are rushing to catch their train. The vendors are busy selling their wares, adding to the vibrant atmosphere of the station. The trains passing by the station add a dynamic element to the scene. The powerful engines pull in and out of the station, their carriages filled with passengers. Each train arrival and departure is a spectacle in itself, signaling a wave of activity on the platform.",
+        },
+    }
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print(response.json())
+    return response.json()["answer"]
+
+
+@bp.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@bp.route('/api/analyze',methods=['POST','GET'])
+
+@bp.route("/api/analyze", methods=["POST", "GET"])
 def dsaResult():
-    if request.method=='POST':
-        text=request.form['text']
-        lst=dsa.ret_results(text)
+    if request.method == "POST":
+        text = request.form["text"]
+        lst = dsa.ret_results(text)
         print(lst)
-        result={'depression':str(lst[0]),'anxiety':str(lst[1]),'stress':str(lst[2])}
+        result = {
+            "depression": str(lst[0]) + "/10",
+            "anxiety": str(lst[1]) + "/10",
+            "stress": str(lst[2]) + "/10",
+        }
+        return jsonify(result)
+
+
+@bp.route("/api/answer", methods=["POST"])
+def getAnswer():
+    if request.method == "POST":
+        text = request.form["question"]
+        answer = answer_based_on_context(text)
+        result = {"answer": answer}
         return jsonify(result)
--- app/dsa.py
+++ app/dsa.py
@@ -29,27 +29,67 @@ warnings.filterwarnings("ignore")
 nltk.download("stopwords")
 nltk.download("wordnet")
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-nlp2 = spacy.load('en_core_web_sm')
-stopword = stopwords.words('english')
-stopword.extend(['take', 'call', 'get', 'first', 'second', 'third', 'need', 'needs', 'dont', 'want', 'give', 'go', 'given', 'turns', 'way', 'i\'m', 'seems', 'place',
-                 'one', 'find', 'told', 'feeling', 'year', 'years', 'told', 'say', 'went', 'got', 'thing', 'take', 'taking', '\'m', '\'re', 'become', 'became', 'made', 'life'])
+nlp2 = spacy.load("en_core_web_sm")
+stopword = stopwords.words("english")
+stopword.extend(
+    [
+        "take",
+        "call",
+        "get",
+        "first",
+        "second",
+        "third",
+        "need",
+        "needs",
+        "dont",
+        "want",
+        "give",
+        "go",
+        "given",
+        "turns",
+        "way",
+        "i'm",
+        "seems",
+        "place",
+        "one",
+        "find",
+        "told",
+        "feeling",
+        "year",
+        "years",
+        "told",
+        "say",
+        "went",
+        "got",
+        "thing",
+        "take",
+        "taking",
+        "'m",
+        "'re",
+        "become",
+        "became",
+        "made",
+        "life",
+    ]
+)
 
 
 def coeff_determination(y_true, y_pred):
-    SS_res = K.sum(K.square(y_true-y_pred))
+    SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return (1 - SS_res/(SS_tot + K.epsilon()))
+    return 1 - SS_res / (SS_tot + K.epsilon())
 
 
 def outer(cell):
     def inner(cell):
-        return WordNetLemmatizer().lemmatize(cell, pos='v')
+        return WordNetLemmatizer().lemmatize(cell, pos="v")
+
     return inner(cell)
 
 
-with open('app/deployables/lemmedWords.pkl', 'rb') as f:
+with open("app/deployables/lemmedWords.pkl", "rb") as f:
     lemmedWords = pickle.load(f)
 antonyms = pd.read_pickle("app/deployables/antonyms.pkl")
 with open("app/deployables/graph.pkl", "rb") as f:
@@ -62,11 +102,17 @@ for i, v in enumerate(lemmedWords):
 
 
 depressionModel = keras.models.load_model(
-    'app/deployables/depression.h5', custom_objects={"coeff_determination": coeff_determination})
+    "app/deployables/depression.h5",
+    custom_objects={"coeff_determination": coeff_determination},
+)
 anxietyModel = keras.models.load_model(
-    'app/deployables/anxiety.h5', custom_objects={"coeff_determination": coeff_determination})
+    "app/deployables/anxiety.h5",
+    custom_objects={"coeff_determination": coeff_determination},
+)
 stressModel = keras.models.load_model(
-    'app/deployables/stress.h5', custom_objects={"coeff_determination": coeff_determination})
+    "app/deployables/stress.h5",
+    custom_objects={"coeff_determination": coeff_determination},
+)
 
 
 def distance(row):
@@ -79,8 +125,7 @@ def distance(row):
     fearEmot = 0
     dist = []
     for i, v in enumerate(row.iloc[1:], 1):
-
-        x = row.iloc[i-1]
+        x = row.iloc[i - 1]
         y = v
         if x not in G.nodes() or y not in G.nodes():
             continue
@@ -88,28 +133,35 @@ def distance(row):
         Emotsum += nx.shortest_path_length(G, x, y)
         dist.append(nx.shortest_path_length(G, x, y))
         if i == 1:
-            happyEmot += nx.shortest_path_length(G, x, 'happy')
-            sadEmot += nx.shortest_path_length(G, x, 'sad')
-            depressionEmot += nx.shortest_path_length(G, x, 'depression')
-            anxietyEmot += nx.shortest_path_length(G, x, 'anxiety')
-            stressEmot += nx.shortest_path_length(G, x, 'stress')
-            fearEmot += nx.shortest_path_length(G, x, 'fear')
-        happyEmot += nx.shortest_path_length(G, y, 'happy')
-        sadEmot += nx.shortest_path_length(G, y, 'sad')
-        depressionEmot += nx.shortest_path_length(G, x, 'depression')
-        anxietyEmot += nx.shortest_path_length(G, x, 'anxiety')
-        stressEmot += nx.shortest_path_length(G, x, 'stress')
+            happyEmot += nx.shortest_path_length(G, x, "happy")
+            sadEmot += nx.shortest_path_length(G, x, "sad")
+            depressionEmot += nx.shortest_path_length(G, x, "depression")
+            anxietyEmot += nx.shortest_path_length(G, x, "anxiety")
+            stressEmot += nx.shortest_path_length(G, x, "stress")
+            fearEmot += nx.shortest_path_length(G, x, "fear")
+        happyEmot += nx.shortest_path_length(G, y, "happy")
+        sadEmot += nx.shortest_path_length(G, y, "sad")
+        depressionEmot += nx.shortest_path_length(G, x, "depression")
+        anxietyEmot += nx.shortest_path_length(G, x, "anxiety")
+        stressEmot += nx.shortest_path_length(G, x, "stress")
         # fearEmot+=nx.shortest_path_length(G,x,'fear')
     counter = Counter(dist)
     probs = []
     for i, v in counter.most_common():
-        probs.append(v/len(dist))
-    return Emotsum, happyEmot, sadEmot, depressionEmot, anxietyEmot, stressEmot, entropy(probs)
+        probs.append(v / len(dist))
+    return (
+        Emotsum,
+        happyEmot,
+        sadEmot,
+        depressionEmot,
+        anxietyEmot,
+        stressEmot,
+        entropy(probs),
+    )
 
 
 def ret_results(text):
-
-    refinednote = re.split('[\n.]', text)
+    refinednote = re.split("[\n.]", text)
     selectedWords = set()
     vector_representation = np.zeros((len(lemmedWords)), dtype=float)
     for sent in refinednote:
@@ -118,15 +170,13 @@ def ret_results(text):
         isNeg = False
 
         for token in note:
-
-            if token.dep_ == 'neg':
+            if token.dep_ == "neg":
                 isNeg = not isNeg
                 count = 0
             if token.text.lower() in stopword or outer(token.text.lower()) in stopword:
                 continue
 
-            if token.pos_ in ['VERB', 'ADJ', 'NOUN']:
-
+            if token.pos_ in ["VERB", "ADJ", "NOUN"]:
                 if isNeg:
                     if count >= 2:
                         count = 0
@@ -135,81 +185,101 @@ def ret_results(text):
                         count += 1
 
                 if isNeg:
+                    sims = np.array(
+                        [
+                            nlp2(outer(token.text.lower())).similarity(nlp2(lemmedWord))
+                            for lemmedWord in lemmedWords
+                        ]
+                    )
 
-                    sims = np.array([nlp2(outer(token.text.lower())).similarity(
-                        nlp2(lemmedWord)) for lemmedWord in lemmedWords])
-
-                    word = antonyms[antonyms[0] == outer(
-                        token.text.lower())][1].values
+                    word = antonyms[antonyms[0] == outer(token.text.lower())][1].values
 
                     if len(word) == 0:
-                        if len(antonyms[antonyms[0] == lemmedWords[sims.argmax()]][1].values) == 0:
+                        if (
+                            len(
+                                antonyms[antonyms[0] == lemmedWords[sims.argmax()]][
+                                    1
+                                ].values
+                            )
+                            == 0
+                        ):
                             continue
                         else:
-
-                            opword = antonyms[antonyms[0] ==
-                                              lemmedWords[sims.argmax()]][1].values[0]
-                            sims = np.array([nlp2(opword).similarity(
-                                nlp2(lemmedWord)) for lemmedWord in lemmedWords])
+                            opword = antonyms[
+                                antonyms[0] == lemmedWords[sims.argmax()]
+                            ][1].values[0]
+                            sims = np.array(
+                                [
+                                    nlp2(opword).similarity(nlp2(lemmedWord))
+                                    for lemmedWord in lemmedWords
+                                ]
+                            )
 
                             selectedWords.add(lemmedWords[sims.argmax()])
                             if sims.max() == 1.0:
-
                                 vector_representation[sims.argmax()] = 0.999
                             else:
-                                vector_representation[sims.argmax(
-                                )] = sims.max()
+                                vector_representation[sims.argmax()] = sims.max()
                             isNeg = False
                             continue
                     else:
-                        sims = np.array([nlp2(word[0]).similarity(
-                            nlp2(lemmedWord)) for lemmedWord in lemmedWords])
+                        sims = np.array(
+                            [
+                                nlp2(word[0]).similarity(nlp2(lemmedWord))
+                                for lemmedWord in lemmedWords
+                            ]
+                        )
 
                         selectedWords.add(lemmedWords[sims.argmax()])
                         if sims.max() == 1.0:
-
                             vector_representation[sims.argmax()] = 0.999
                         else:
                             vector_representation[sims.argmax()] = sims.max()
                         isNeg = False
                         continue
                 if outer(token.text.lower()) in lemmedWords:
-
-                    vector_representation[lemmedWords.index(
-                        outer(token.text.lower()))] = 0.99
+                    vector_representation[
+                        lemmedWords.index(outer(token.text.lower()))
+                    ] = 0.99
                     selectedWords.add(token.text.lower())
                     continue
                 if outer(token.text.lower()) in G.nodes():
-
-                    neibs = [i for i in nx.neighbors(
-                        G, outer(token.text.lower())) if i in lemmedWords]
+                    neibs = [
+                        i
+                        for i in nx.neighbors(G, outer(token.text.lower()))
+                        if i in lemmedWords
+                    ]
 
                     if len(neibs) == 0:
                         continue
                     sims = np.array(
-                        [nlp2(outer(token.text.lower())).similarity(nlp2(neib)) for neib in neibs])
+                        [
+                            nlp2(outer(token.text.lower())).similarity(nlp2(neib))
+                            for neib in neibs
+                        ]
+                    )
                     if sims.max() < 0.5:
-
                         continue
                     selectedWords.add(neibs[sims.argmax()])
 
                     if sims.max() == 1.0:
-
-                        vector_representation[lemmedIndex[neibs[sims.argmax()]]
-                                              ] = 0.999
+                        vector_representation[lemmedIndex[neibs[sims.argmax()]]] = 0.999
                     else:
-                        vector_representation[lemmedIndex[neibs[sims.argmax()]]] = sims.max(
-                        )
+                        vector_representation[
+                            lemmedIndex[neibs[sims.argmax()]]
+                        ] = sims.max()
                 else:
-
-                    sims = np.array([nlp2(outer(token.text.lower())).similarity(
-                        nlp2(lemmedWord)) for lemmedWord in lemmedWords])
+                    sims = np.array(
+                        [
+                            nlp2(outer(token.text.lower())).similarity(nlp2(lemmedWord))
+                            for lemmedWord in lemmedWords
+                        ]
+                    )
                     if sims.max() < 0.5:
                         continue
                     selectedWords.add(lemmedWords[sims.argmax()])
 
                     if sims.max() == 1.0:
-
                         vector_representation[sims.argmax()] = 0.999
                     else:
                         vector_representation[sims.argmax()] = sims.max()
@@ -218,16 +288,31 @@ def ret_results(text):
     testSample = []
 
     if len(selectedWords) != 0:
-
         testWords = np.array(list(selectedWords))
         testWords = testWords.reshape((1, -1))
         test = pd.DataFrame(testWords)
-        test['EmotionSum'] = test.apply(lambda row: distance(row), axis=1)
-        test[['EmotionSum', 'happyEmotions', 'SadEmotions', 'depressedEmotions', 'anxietyEmotions',
-              'stressEmotions', 'entropy']] = pd.DataFrame(test.EmotionSum.to_list(), index=test.index)
+        test["EmotionSum"] = test.apply(lambda row: distance(row), axis=1)
+        test[
+            [
+                "EmotionSum",
+                "happyEmotions",
+                "SadEmotions",
+                "depressedEmotions",
+                "anxietyEmotions",
+                "stressEmotions",
+                "entropy",
+            ]
+        ] = pd.DataFrame(test.EmotionSum.to_list(), index=test.index)
         test = test.iloc[:, -7:]
-        testSample = pd.concat([pd.DataFrame(vector_representation.reshape(
-            1, -1), columns=flatDF_weight.columns), test], axis=1)
+        testSample = pd.concat(
+            [
+                pd.DataFrame(
+                    vector_representation.reshape(1, -1), columns=flatDF_weight.columns
+                ),
+                test,
+            ],
+            axis=1,
+        )
 
         depression = depressionModel.predict(testSample.values)[0][0]
         anxiety = anxietyModel.predict(testSample.values)[0][0]
@@ -235,16 +320,14 @@ def ret_results(text):
 
     return [depression, anxiety, stress]
 
+
 def analyze():
-    while(True):
+    while True:
         text = input("Enter text you want  to analyze or enter exit to terminate")
-        if text.lower() == 'exit':
+        if text.lower() == "exit":
             sys.exit()
 
         d, a, s = ret_results(text)
-        print(f'depression={d}')
-        print(f'anxiety={a}')
-        print(f'stress={s}')
-
-def test():
-    return 'Hello flask'
+        print(f"depression={d}")
+        print(f"anxiety={a}")
+        print(f"stress={s}")
